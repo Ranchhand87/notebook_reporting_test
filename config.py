0b5d6dfff451d841ea19d3cb68097a0b829a6fdf
Tex M. <164884889+Ranchhand87@users.noreply.github.com>
2024-12-12 07:28:19 -0600

    copy notebook backend

---
+++ config.py
@@ -0,0 +1,11 @@
+credentials = {'username': 'terence.mccutcheon',
+               'password': 'Wespen24!'}
+
+## connection parameters
+host = 'intersectmbo.biterg.io'
+port = '443'
+path = 'data'
+use_ssl = True
+verify_certs = False
+timeout = 60
+max_retries = 2
